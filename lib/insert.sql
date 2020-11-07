2e4aa8d47349305c8fd0fa02e15c9fd925547fce
kelseamc <kelsea.mcall@gmail.com>
2020-11-06 20:43:45 -0500

    finished

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Sleepy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'Black', 'Crazy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'White', 'Happy', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'F', 'Grumpy', 'Sleepy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 20, 'F', 'Brown', 'Lazy', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 14, 'M', 'Black', 'Friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'Brown', 'Nice', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, 'M', 'Black', 'Creepy', true);
